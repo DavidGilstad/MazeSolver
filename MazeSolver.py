795dc4787ce7856ba447517ce7ee41d5846d196b
David Gilstad <david.gilstad@ou.edu>
2020-10-10 16:40:17 -0500

    implement greedy action select methods

--- MazeSolver.py
+++ MazeSolver.py
@@ -10,7 +10,7 @@ import numpy as np
 #	authors: David Gilstad, Ian Schwind
 #	created: October 9, 2020
 #
-alpha, gamma = 0.05, 0.9 # TODO: Find out what are good values and explain in paper
+alpha, epsilon, gamma = 0.05, 0.1, 0.9 # TODO: Find out what are good values and explain in paper
 height = width = 6
 
 # locations of the following objects on the grid
@@ -29,23 +29,25 @@ class TD_agent:
     
     # check what value is at state of next move
     def getleft(self):
-        if self.curr_loc[0] == 0: return -1
+        if self.curr_loc[0] == 0: return Rwall
         else: return self.v_values[self.curr_loc[0]-1, self.curr_loc[1]]
 
     def getright(self):
-        if self.curr_loc[0] == width: return -1
+        if self.curr_loc[0] == width: return Rwall
         else: return self.v_values[self.curr_loc[0]+1, self.curr_loc[1]]
 
     def getup(self):
-        if self.curr_loc[1] == height: return -1
+        if self.curr_loc[1] == height: return Rwall
         else: return self.v_values[self.curr_loc[0], self.curr_loc[1]+1]
 
     def getdown(self):
-        if self.curr_loc[1] == 0: return -1
+        if self.curr_loc[1] == 0: return Rwall
         else: return self.v_values[self.curr_loc[0], self.curr_loc[1]-1]
 
+    # movements: move to the selected state, reward the start state
+    # based on 
     def left(self):
-        pass
+        if self.curr_loc[0] == 0: return 
 
     def right(self):
         pass
@@ -58,11 +60,24 @@ class TD_agent:
 
     # determines the probability of going into the next state based on the value
     # at that state already. gives higher probability to higher value states.
-    def action_probs(self, epsilon=0.1): # TODO: explain epsilon value choice
+    def action_probs(self): # TODO: explain epsilon value choice
         probs = [self.getleft(),self.getright(),self.getup(),self.getdown()]
         probs = probs - min(probs) + epsilon
         return probs / probs.sum()
 
+    # pick the action 
+    def greedy(self):
+        values = [self.getleft(),self.getright(),self.getup(),self.getdown()]
+        return self.actions[values.index(max(values))]
+
+    # try a random option
+    def explore(self):
+        return self.actions[np.random.choice([0,1,2,3])]
+
+    # choose an action greedily, i.e. the max value action with probability
+    # 1-epsilon, and a random action with probability epsilon
+    def action(self):
+        np.random.choice([self.greedy(),self.explore()], p=[1-epsilon,epsilon])()
     
 
 # Really cool method!
@@ -70,3 +85,6 @@ class TD_agent:
 # probabilities, then the extra () at the end uses the name to call
 # that method!
 # reward, action_idx = np.random.choice(self.actions, p=action_probs)()
+#
+#values = self.getValues()
+        #return self.actions[values.index(max(values))]

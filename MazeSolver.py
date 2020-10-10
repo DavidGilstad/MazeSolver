c47f9a1fd7f8736184c668cab8470c2b1a1fe761
David Gilstad <david.gilstad@ou.edu>
2020-10-10 18:24:41 -0500

    clean up code to prepare for second phase of simulation

--- MazeSolver.py
+++ MazeSolver.py
@@ -1,16 +1,15 @@
-import numpy as np
-
 #
 # Maze Solver
 #
 # Machine Learning project on reinforcement learning by teaching
 # an agent how to go through a maze in optimal steps without running
 # into objects along the way
-
 #
 #   authors: David Gilstad, Ian Schwind
 #   created: October 9, 2020
 #
+import numpy as np
+
 alpha, epsilon, gamma = 0.05, 1, 0.9 # TODO: Find out what are good values and explain in paper
 height = width = 6
 
@@ -63,13 +62,6 @@ class TD_agent:
         else: # do action
             self.prev_loc, self.curr_loc[1] = self.curr_loc, self.curr_loc[1] + 1
 
-    # determines the probability of going into the next state based on the value
-    # at that state already. gives higher probability to higher value states.
-    # def action_probs(self): 
-    #    probs = self.gevValues()
-    #   probs = probs - min(probs) + epsilon
-    #   return probs / probs.sum()
-
     # pick the action 
     def greedy(self):
         values = self.getValues()
@@ -94,11 +86,4 @@ if __name__ == '__main__':
         
     print("ended at:",a.curr_loc)
     print("number of iterations:",count)
-
- # idea to get rid of different actions
- # 
- # generic move has [coordinate,movement,check] where coordinate is
- # which axis it affects (x=0 or y=1), movement is direction (+/- 1),
- # and check is value to do boundary check on (0, width, height)
- 
- 
+    

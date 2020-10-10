95ec42871600ac5f325eba88175aa012689d6ced
David Gilstad <david.gilstad@ou.edu>
2020-10-10 00:45:08 -0500

    add dimensions, object locations, rewards, and learning method values

--- MazeSolver.py
+++ MazeSolver.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 #
 # Maze Solver
 #
@@ -8,3 +10,14 @@
 #	authors: David Gilstad, Ian Schwind
 #	created: October 9, 2020
 #
+alpha, gamma = 0.05, 0.9 # TODO: Find out what are good values and explain in paper
+height = width = 6
+
+# locations of the following objects on the grid
+start = [0,5]
+end = [5,4]
+walls = [[2,2],[2,3],[3,5],[4,3],[4,4]]
+
+min_steps = 10 # minimum number of steps from start to finish
+Rend, Rwall, Rdistance = 5, -1, 0.5 # reward values
+
